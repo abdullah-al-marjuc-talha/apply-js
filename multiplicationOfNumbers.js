4af232e6956373f0f7d79b400f2894475b0138b6
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-20 15:57:34 +0600

    factorial added

---
+++ multiplicationOfNumbers.js
@@ -0,0 +1,15 @@
+// multiplicationOfNumbers.js
+//factorial
+function multiplicationOfNumbers(number)
+{
+    let result = 1;
+    for(let i=1; i<=number; i++)
+    {
+        result= result*i;
+        
+    }
+    return result
+}
+
+const result = multiplicationOfNumbers(7);
+console.log(result);
