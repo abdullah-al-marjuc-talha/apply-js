ddb7eb28f313526b6d04f51ee0812bb55374496b
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-20 15:49:35 +0600

    sum of numbers added

---
+++ sumOfNumbers.js
@@ -0,0 +1,18 @@
+// let sum = 0;
+
+// for (let i = 1; i<=7; i++)
+// {
+//     sum = sum + i;
+//     console.log(i, sum);
+// }
+
+function sumOfNumbers(number) 
+{
+    let sum = 0;
+    for (let i = 1; i <= 7; i++) 
+    {
+        sum = sum + i;
+        console.log(i, sum);
+    }
+    return sum;
+}
