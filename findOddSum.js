3c3884b983fecd4be719494e8e2e046f8ce9e2a9
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-20 15:44:02 +0600

    odd numbers sum added

--- findOddSum.js
+++ findOddSum.js
@@ -7,10 +7,36 @@ function getSumOfAnArray(numbers)
         const index = i ;
         const element = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
 
     }
     return sum;
 }
+
+function getOddNumbersOfAnArray(numbers)
+{
+    const oddNumbers = [];
+    for(let i=0; i<numbers.length;i++)
+    {
+        // console.log(i);
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0)
+        {
+            console.log(index,element);
+            oddNumbers.push(element);
+        }
+        
+    }
+
+    return oddNumbers;
+    
+}
+
 const myNumbers = [12,65,45,78,32,45,91];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
 getSumOfAnArray(myNumbers);
+
+const oddNumberSum = getSumOfAnArray(oddNumbers);
+console.log('odd numbers sum: ', oddNumberSum);
